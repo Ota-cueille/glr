fe851650ffda4314c1d9bd1fefd2b2bb32ae5f44
Tom Chauvel <tom.chauvel@proton.me>
2024-04-23 21:55:09 +0200

    KEYS (#6)
    * keys added

--- include/events.hpp
+++ include/events.hpp
@@ -5,11 +5,23 @@
 
 namespace event {
 
-    enum class type { close, resize, refresh, size };
+    enum class type { close, resize, refresh, key, size };
     
+    namespace key {
+        enum class action {press, release, keep};
+        enum class mods {ctrl, alt, shift, caps, num};
+    }
+
     struct Event {
         type t;
         union {
+            // key events data
+            struct{
+                int keycode, scancode;
+                key::action action;
+                key::mods mods;               
+            } key;
+
             // resize events data
             struct {
                 u32 width, height;
